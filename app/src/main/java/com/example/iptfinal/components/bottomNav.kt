419a60f0484588ec733adc00346f93158724b84c
JudeMichaelArriba <arribajudemichael@gmail.com>
2025-09-24 16:06:37 +0800

    fragmentsAdded

--- app/src/main/java/com/example/iptfinal/components/bottomNav.kt
+++ app/src/main/java/com/example/iptfinal/components/bottomNav.kt
@@ -6,23 +6,53 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.iptfinal.R
+import com.example.iptfinal.databinding.ActivityBottomNavBinding
 import com.example.iptfinal.databinding.ActivityMainBinding
 
 class bottomNav : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityBottomNavBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityBottomNavBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomNavigator)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.bottomNav.selectedItemId = R.id.home
+
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.home ->{
+                    true
+                }
+                R.id.baby ->{
+                    true
+                }
+                R.id.notification ->{
+                    true
+                }
+                R.id.profile ->{
+                    true
+                }else -> false
+            }
+        }
+
+
+
     }
+
+
+
+
+
 }
